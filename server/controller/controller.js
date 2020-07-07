74a27f54e74f4e1e5f0f22bb8fdc7c0b54507fc1
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-07-06 21:15:21 -0300

    Create login user

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,11 +1,16 @@
 import User from "../models/UserModel";
+import bcrypt from "bcryptjs";
 
 const controller = {
   register: async (req, res) => {
+    const selectedUser = await User.findOne({ email: req.body.email });
+    if (selectedUser) {
+      return res.status(400).send("Email already exists");
+    }
     const user = new User({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: bcrypt.hashSync(req.body.password),
     });
     try {
       const saveUser = await user.save();
@@ -14,8 +19,19 @@ const controller = {
       res.status(400).send(error);
     }
   },
-  login: (req, res) => {
-    res.send("Register");
+  login: async (req, res) => {
+    const loginUser = await User.findOne({ email: req.body.email });
+    if (!loginUser) {
+      return res.status(400).send("Email or Password incorrect");
+    }
+    const validPassword = bcrypt.compareSync(
+      req.body.password,
+      loginUser.password
+    );
+    if (!validPassword) {
+      return res.status(400).send("Email or Password incorrect");
+    }
+    return res.status(200).send("Ok");
   },
 };
 
